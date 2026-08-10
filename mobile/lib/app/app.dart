60e91301cc8d03791686f6bf1f7b3ed8d2879f62
peya025 <piasarzuelo@gmail.com>
2026-08-10 14:03:00 +0800

    Restore mobile app entry widget app.dart deleted by mistake

---
+++ mobile/lib/app/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'app_router.dart';
+import 'app_routes.dart';
+import 'app_theme.dart';
+import '../services/api.dart';
+
+class AnimalCareApp extends StatelessWidget {
+  const AnimalCareApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animal Bite Management System',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      initialRoute: api.isAuthenticated
+          ? AppRoutes.menu
+          : AppRoutes.welcome,
+      onGenerateRoute: AppRouter.onGenerateRoute,
+    );
+  }
+}
